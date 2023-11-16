7c21bfc8555df094a3508c40b701dd364beb69e7
Luciana Sonoda <142545698+LittleSkullNerd@users.noreply.github.com>
2023-11-16 16:30:16 -0300

    alura_imersaodev7_aula3

---
+++ script.js
@@ -0,0 +1,12 @@
+var listaFilmes = ['https://upload.wikimedia.org/wikipedia/pt/thumb/1/1b/Schoolrockposter.jpg/210px-Schoolrockposter.jpg', 
+                   'https://media.fstatic.com/DjhdaLmpvfJaleMFTvM2aN1-zq8=/322x478/smart/filters:format(webp)/media/movies/covers/2016/08/chegada_2-1.jpg', 
+                   'https://musicart.xboxlive.com/7/94fc5000-0000-0000-0000-000000000002/504/image.jpg', 
+                   'https://upload.wikimedia.org/wikipedia/pt/7/76/10_Things_I_Hate_About_You.jpg', 'https://upload.wikimedia.org/wikipedia/pt/c/c1/The_Matrix_Poster.jpg']
+
+// listaFilmes[0] = ;
+// listaFilmes[1] = ;
+// listaFilmes[2] = ;
+
+for(var i = 0; i < listaFilmes.length; i++){
+  document.write("<img src=" + listaFilmes[i] + '>')
+} 
